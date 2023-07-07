688901dfe4303c1065bbfa185eb979d49440fa67
Quentin Dequippe <quentin.dequippe@tiime.fr>
2023-07-07 20:02:55 +0400

    Remove useless config

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace QdequippeTech\Silae;
 
-use Http\Client\Common\Plugin\AddHostPlugin;
-use Http\Client\Common\Plugin\AddPathPlugin;
-use Http\Client\Common\PluginClient;
-use Http\Discovery\Psr17FactoryDiscovery;
-use Http\Discovery\Psr18ClientDiscovery;
 use Jane\Component\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
-use Psr\Http\Client\ClientInterface;
 use QdequippeTech\Silae\Api\Client;
 use QdequippeTech\Silae\HttpPlugin\OAuth2AuthenticationPlugin;
 
@@ -19,23 +13,8 @@ final class ClientFactory
     public static function create(
         string $clientId,
         string $clientSecret,
-        string $baseUri = 'https://payroll-api.silae.fr',
-        ClientInterface $httpClient = null
     ): Client
     {
-        // Find a default HTTP client if none provided
-        if (null === $httpClient) {
-            $httpClient = Psr18ClientDiscovery::find();
-        }
-
-        // Decorates the HTTP client with some plugins
-        $hostUri = Psr17FactoryDiscovery::findUriFactory()->createUri($baseUri);
-        $pathUri = Psr17FactoryDiscovery::findUriFactory()->createUri($baseUri.'/payroll');
-        $pluginClient = new PluginClient($httpClient, [
-            new AddHostPlugin($hostUri),
-            new AddPathPlugin($pathUri)
-        ]);
-
         $authenticationRegistry = new AuthenticationRegistry([new OAuth2AuthenticationPlugin([
             'clientId' => $clientId,
             'clientSecret' => $clientSecret,
@@ -44,6 +23,6 @@ final class ClientFactory
             'urlResourceOwnerDetails' => '',
         ])]);
 
-        return Client::create($pluginClient, [$authenticationRegistry]);
+        return Client::create(null, [$authenticationRegistry]);
     }
 }
