00171961b106db8e6722f617defcbc8e9622fd43
Quentin Dequippe <quentin.dequippe@tiime.fr>
2023-08-07 18:35:01 +0400

    Add authenticate method custom Client

---
+++ src/Client.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QdequippeTech\Silae;
+
+use Http\Discovery\Psr17FactoryDiscovery;
+use Psr\Http\Client\ClientExceptionInterface;
+use QdequippeTech\Silae\Api\Client as ApiClient;
+use QdequippeTech\Silae\Model\AccessToken;
+
+final class Client extends ApiClient
+{
+    /**
+     * @throws ClientExceptionInterface
+     */
+    public function authenticate(
+        string $clientId,
+        string $clientSecret,
+        string $grantType = 'client_credentials',
+        string $scope = 'https://silaecloudb2c.onmicrosoft.com/36658aca-9556-41b7-9e48-77e90b006f34/.default',
+    ): ?AccessToken {
+        $uri = Psr17FactoryDiscovery::findUriFactory()->createUri('https://payroll-api-auth.silae.fr/oauth2/v2.0/token');
+        $uri = $uri->withQuery(http_build_query([
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'grant_type' => $grantType,
+            'scope' => $scope,
+        ]));
+        $authenticationRequest = $this->requestFactory->createRequest('POST', $uri);
+        $authenticationRequest = $authenticationRequest->withHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $response = $this->httpClient->sendRequest($authenticationRequest);
+
+        if (200 === $response->getStatusCode()) {
+            $data = json_decode((string) $response->getBody(), true);
+
+            return new AccessToken(
+                $data['access_token'],
+                $data['token_type'],
+                $data['not_before'],
+                $data['expires_in'],
+                $data['expires_on'],
+                $data['resource'],
+            );
+        }
+
+        return null;
+    }
+}
---
+++ src/ClientFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QdequippeTech\Silae;
+
+use Psr\Http\Client\ClientInterface;
+
+final class ClientFactory
+{
+    public static function create(ClientInterface $httpClient = null): Client
+    {
+        return Client::create($httpClient);
+    }
+}
